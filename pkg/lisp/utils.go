3d09b3abd403c6abb70df1d10601cf4066f43317
JairAntonio22 <jair.loranca@gmail.com>
2023-09-17 18:25:39 -0600

    removed unused function

--- pkg/lisp/utils.go
+++ pkg/lisp/utils.go
@@ -1,21 +1,9 @@
 package lisp
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func handle(err *error) {
 	if r := recover(); r != nil {
 		*err = fmt.Errorf("error: %s", r)
 	}
-}
-
-func mustParseInt(a Expr) int64 {
-	num, err := strconv.ParseInt(a.String(), 10, 64)
-	if err != nil {
-		panic("could not read integer from given value:" + a.String())
-	}
-
-	return num
 }
